b9bce53bc3d2657844324f3cb87cdadbfea77fa0
Viggen <koriremanuel0@gmail.com>
2024-07-15 16:45:04 +0300

    Update

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// solution
+var customerName = 'bob';
+const leastFavoriteCustomer = '';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Joe';
+}
+customerName()
+console.log(customerName);
+upperCaseCustomerName();
+console.log(customerName);
+setBestCustomer();
+console.log(bestCustomer);
+overwriteBestCustomer();
+console.log(bestCustomer);
+// changeLeastFavoriteCustomer();
+// console.log(leastFavoriteCustomer)
